bbbf21516f51d177fda7f446de70f10cd9b40e8d
SNIVY <870667674@qq.com>
2022-08-05 15:31:09 +0800

    播放器优化前保存

--- src/store/player.js
+++ src/store/player.js
@@ -3,10 +3,39 @@ import { songUrl } from "@/api"
 export default {
     namespaced: true,
     actions: {
+        main() {//在main.js调用
+            this.bgAudioManager.playbackRate = 2
+            this.bgAudioManager.onPlay(() => {
+                this.isPlayingMusic = true
+                this.playingMusicId = this.bgAudioManager.musicID
+            })
+            this.bgAudioManager.onPause(() => {
+                this.isPlayingMusic = false
+            })
+            this.bgAudioManager.onStop(() => {
+                this.isPlayingMusic = false
+            })
+            this.bgAudioManager.onEnded(() => {
+                this.isPlayingMusic = false
+                if (this.playList.length) {
+                    songUrl(this.playList[0].musicID).then(res => {
+                        const data = res.data.data[0]
+                        this.bgAudioManager.musicID = data.id
+                        this.bgAudioManager.src = data.url
+                        this.bgAudioManager.singer = this.playList[0].singer
+                        this.bgAudioManager.title = this.playList[0].title
+                        this.bgAudioManager.coverImgUrl = this.playList[0].coverImgUrl
+                    }).then(() => {
+                        this.bgAudioManager.play()
+                        this.playList.shift()
+                    })
+                }
+            })
+        },
         updateMusicFromPlayList(context, musicLists) {//接收存放音乐数据的数组，不用包含播放路径
             context.dispatch('startPlayMusic', musicLists[0])
             musicLists.shift()
-            context.state=[]
+            context.state = []
             context.commit('addMusicFromPlayList', musicLists)
         },
         startPlayMusic(context, obj) {
@@ -43,35 +72,6 @@ export default {
         //#endif
         playList: [],//播放列表，存放音乐数据的对象
         isPlayingMusic: false,//判断音乐是否播放，只在main.js设置
-        playingMusicId:0,//播放中的音乐的ID
-        main() {//在main.js调用
-            this.bgAudioManager.playbackRate=2
-            this.bgAudioManager.onPlay(() => {
-                this.isPlayingMusic = true
-                this.playingMusicId=this.bgAudioManager.musicID
-            })
-            this.bgAudioManager.onPause(() => {
-                this.isPlayingMusic = false
-            })
-            this.bgAudioManager.onStop(() => {
-                this.isPlayingMusic = false
-            })
-            this.bgAudioManager.onEnded(() => {
-                this.isPlayingMusic = false
-                if (this.playList.length) {
-                    songUrl(this.playList[0].musicID).then(res => {
-                        const data = res.data.data[0]
-                        this.bgAudioManager.musicID = data.id
-                        this.bgAudioManager.src = data.url
-                        this.bgAudioManager.singer = this.playList[0].singer
-                        this.bgAudioManager.title = this.playList[0].title
-                        this.bgAudioManager.coverImgUrl = this.playList[0].coverImgUrl
-                    }).then(() => {
-                        this.bgAudioManager.play()
-                        this.playList.shift()
-                    })
-                }
-            })
-        }
+        playingMusicId: 0,//播放中的音乐的ID
     }
 }
